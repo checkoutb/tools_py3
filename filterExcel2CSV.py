764bcc87d9e28f87596b99cd9cd5282459ebb8ab
wb <f12.628@hotmail.com>
2021-11-29 08:41:34 +0800

    2021-11-29

--- filterExcel2CSV.py
+++ filterExcel2CSV.py
@@ -1,3 +1,4 @@
+from typing import ItemsView
 import xlrd
 import csv
 # import xlwt
@@ -7,14 +8,14 @@ import time
 from xlrd.sheet import Cell
 
 
-excel_path = "/home/ubuntu20/Documents/aa.xls"
-out_path = "/home/ubuntu20/Documents/csvs/"
+excel_path = "/home/ubuntu20/Documents/aa.xls"              # excel文件的 路径
+out_path = "/home/ubuntu20/Documents/csvs/"            # csv文件的 目录
 
 class LineInfo:
-    id = ""
-    name = ""
+    id = ""         # video id
+    name = ""       # video name
     category = ""       # app tag
-    template = ""           # fengbian
+    template = ""           # 类型/分辨率
     url = ""
     updatetime = ""
 
@@ -32,13 +33,13 @@ def readAndDistinct():
 
     book = xlrd.open_workbook(excel_path)
     print("sz of sheets:" + str(book.nsheets))
-    for i in range(0, book.nsheets):
+    for i in range(0, book.nsheets):              # 遍历每个sheet
         sh = book.sheet_by_index(i)
         print("sz of rows:" + str(sh.nrows))
         print(sh.row(0))
         sz2 = sh.ncols
         print(sz2)
-        if len(head) == 0:
+        if len(head) == 0:             # 这个是 构造 csv的 首行，是 通过 excel的 首行 + 前缀 来生成  csv的首行
             row = sh.row(0)
             # print(row.count)
             # for v in row:
@@ -46,9 +47,9 @@ def readAndDistinct():
             for i in range(0, sz2):
                 head.append(suffix[i] + row[i].value)
             print("head. ", head)
-        for r in range(1, sh.nrows):
+        for r in range(1, sh.nrows):              # 遍历 sheet。
             ll = sh.row(r)
-            info = convert(ll)
+            info = convert(ll)          # 一行转成 一个对象
             alllist.append(info)
             idset.add(info.id)
             cateset.add(info.category)
@@ -62,12 +63,13 @@ def readAndDistinct():
                 tempmap[info.template] = set()
             if not (info.template in tempInfoMap):
                 tempInfoMap[info.template] = []
-            catemap[info.category].add(info.id)
-            tempmap[info.template].add(info.id)
-            tempInfoMap[info.template].append(info)
+            catemap[info.category].add(info.id)         # 按 app_tag 分组， 存的是 id， 为了print
+            tempmap[info.template].add(info.id)     # 按 分辨率 分组， 存id， 为了 print 和 过滤
+            tempInfoMap[info.template].append(info)  # 按分辨路分组， 存对象， 为了 csv文件
 
 
 
+# 下面是 写代码时的 一些 输出， 来参考这些输出，写的代码。 没有什么用了
     print(len(alllist), len(idset), len(cateset), len(tempset))
     # print(idset)
     print(cateset)
@@ -107,19 +109,23 @@ def readAndDistinct():
                 seta = v & v2
                 if len(seta) > 0:
                     print("aaa. ", k, seta)
+# 输出的结束，上面没有什么用。 也不太清楚作用了。
 
+
+
+    # 开始获得 csv文件中的 超请，高清，标清，流畅，并 过滤
     arr = ['超清','高清','标清','流畅']
     newList = []
-    gotset = set()
-    for tp in arr:
+    gotset = set()          # 已经获得的 videoID
+    for tp in arr:            # 遍历arr，这样就 先获得 超请，再高清， 再标清，最后 流畅
         infoarr = tempInfoMap[tp]
         print(tp, len(infoarr), len(newList))
         for info in infoarr:
-            if not info.id in gotset:
+            if not info.id in gotset:            # 如果这个videoId 没有访问过，说明是 这个video的 最高分辨率。
                 if tp == '超清':
                     print(info.template, "  tp ", len(newList))
                 newList.append(info)
-        gotset = gotset | tempmap[tp]
+        gotset = gotset | tempmap[tp]            # 该分辨率的 所有id  都已经访问， 加入到 已访问集合
         print(len(newList))
     for k, v in tempInfoMap.items():
         if not k in arr:
@@ -129,14 +135,15 @@ def readAndDistinct():
     print(len(newList), len(gotset))
     print(newList[0])
 
-    newList = sorted(newList, key=lambda x : x.updatetime)
+    newList = sorted(newList, key=lambda x : x.updatetime)         # 按 updatetime 倒序
 
+# 下面只是 超请，高清，标清，流畅 的 csv
     csvfn = out_path + "asd"
     cnt = 1
     lstnum = 0
     num = 5000
     while lstnum <= len(newList):
-        part = newList[lstnum : num]
+        part = newList[lstnum : num]              # 每次5000条，在本段的最后修改。
         with open(csvfn + str(cnt).zfill(3) + '.csv', 'w', newline='') as csvFile:
             # sw = csv.writer(csvFile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             sw = csv.writer(csvFile)
@@ -146,13 +153,26 @@ def readAndDistinct():
                 # if info.template != '流畅':
                 #     print(info.template)
                 sw.writerow([info.id, info.name, info.category, info.template, info.url, info.updatetime])
-        cnt += 1
+        cnt += 1            # csv文件名字的一部分
         lstnum = num
-        num += 5000
+        num += 5000         # +5000
+
+# 下面是 非 超请，标情，高清，流畅 的 csv， 这里没有过滤。
+    newList = []
+    for k, v in tempInfoMap.items():
+        if not k in arr:
+            for info in v:
+                newList.append(info)
+    print("...", len(newList))
+    with open(csvfn + '_other.csv', 'w', newline='') as csvFile:              # 数量比较少，所以直接一个文件
+        sw = csv.writer(csvFile)
+        sw.writerow(head)
+        for info in newList:
+            sw.writerow([info.id, info.name, info.category, info.template, info.url, info.updatetime])
+    
 
 
-
-
+# excel中 每行数据转换为 LineInfo对象。
 def convert(ll: list) -> LineInfo:              # list != List
     info = LineInfo()
     info.id = ll[0].value
